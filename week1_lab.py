32e1fbcad9aab91874f8a23d9b72aa5b1ab5d758
Theeah <sisterOfSAT@zoho.eu>
2025-10-09 12:48:24 +0100

    Update week1_lab.py

--- week1_lab.py
+++ week1_lab.py
@@ -1,6 +1,19 @@
+# Exercise 3
 print ("Hello World")
 
+# Exercise 4
 me=("Feeyah")
 age=(18)
 print("Hi, I'm",me,"and I'm ",age,".")
 print("In two years I will be",age+2)
+
+# Challenge
+nameRightBorder=f"{"|":>7}"
+userRightBorder=f"{"|":>10}"
+locRightBorder=f"{"|":>8}"
+
+print("+---------------------------+")
+print( "| Name: Sophia Tingle",nameRightBorder)
+print("| Username: Feeyah",userRightBorder)
+print("| Location: Ormskirk",locRightBorder)
+print("+---------------------------+")
