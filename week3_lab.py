3635dfc3a5196998f97f1291172ae516b077b7be
Sophia Tingle <26481529@edgehill.ac.uk>
2025-10-22 11:07:50 +0100

    Added a calculator thing

--- week3_lab.py
+++ week3_lab.py
@@ -15,3 +15,38 @@ for i in range(5):
     for j in range(i+1):
         print("*",end="")
     print()
+
+loop=True
+while(loop==True):
+    print("What would you like to do?")
+    print("Type 1 to add some numbers!")
+    print("Type 2 to subtract some numbers!")
+    print("Type 3 to exit the calculator!")
+    decision=input()
+    if(decision=="3"):
+        print("Thank you for using the calculator!")
+        loop=False
+    elif(decision=="1"):
+        num1=0
+        num2=0
+        try:
+            num1=int(input("Input your first number"))
+            num2=int(input("Input your second number"))
+        except:
+            print("Fail.")
+        
+        print(f"The answer is {num1+num2}")
+        print()
+    elif(decision=="2"):
+        num1=0
+        num2=0
+        try:
+            num1=int(input("Input your first number"))
+            num2=int(input("Input your second number"))
+        except:
+            print("Fail.")
+        
+        print(f"The answer is {num1-num2}")
+        print()
+    else:
+        print()
